dfcf4afd0b7dacc93994ad362ed66c42548cb666
Magalie Allard <magalieallard@icloud.com>
2021-02-18 11:39:04 -0500

    Add features list component
    Link it to home and subscription

--- src/client/src/scenes/Account/Subscription.js
+++ src/client/src/scenes/Account/Subscription.js
@@ -4,6 +4,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import SubscribeForm from "./SubscribeForm";
 import Button from "../../components/Form/Button";
+import FeaturesList from "../../components/Content/FeaturesList";
 
 export default function Subscription(props) {
   const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -26,21 +27,32 @@ export default function Subscription(props) {
     }
   }
 
-  const pricing = (
+  const productPresentation = (
     <div className="mt-4">
       <p>{props.translate("By subscribing to Calendarium, you will be able to create and manage your own calendars, and invite people to collaborate with you on these calendars.")}</p>
-      <div className="card mt-4" style={{ width: "18rem" }}>
-        {/* TODO: create a header image for this subscription product */}
-        <svg className="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="26%" y="50%" fill="#dee2e6" dy=".3em">Image placeholder</text></svg>
-        <div className="card-body">
-          <h5 className="card-title">{props.translate("Calendarium Unlimited")}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{props.translate("$600 / year")}</h6>
-          <p className="card-text">{props.translate("Includes unlimited calendars and unlimited additional users.")}</p>
-          <Button label={props.translate("Subscribe")} type="button" id="button-subscribe" onClick={(e) => subscribe(e)} />
+      <p>
+        <Button label={props.translate("What can Calendarium do?")} id="button-features-list" type="button" dataBsToggle="collapse" dataBsTarget="#features-list" ariaExpanded="false" ariaControls="features-list" />
+      </p>
+      <div className="collapse" id="features-list">
+        <div className="card card-body">
+          <FeaturesList translate={props.translate} />
         </div>
       </div>
     </div>
-  )
+  );
+
+  const pricing = (
+    <div className="card mt-4" style={{ width: "18rem" }}>
+      {/* TODO: create a header image for this subscription product */}
+      <svg className="card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="26%" y="50%" fill="#dee2e6" dy=".3em">Image placeholder</text></svg>
+      <div className="card-body">
+        <h5 className="card-title">{props.translate("Calendarium Unlimited")}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{props.translate("$600 / year")}</h6>
+        <p className="card-text">{props.translate("Includes unlimited calendars and unlimited additional users.")}</p>
+        <Button label={props.translate("Subscribe")} type="button" id="button-subscribe" onClick={(e) => subscribe(e)} />
+      </div>
+    </div>
+  );
 
   function renderMain() {
     let result = null;
@@ -51,17 +63,25 @@ export default function Subscription(props) {
         if (wantToSubscribe) {
           // Show subscribe form
           result = (
-            <Elements stripe={stripePromise}>
-              <SubscribeForm
-                createSubscription={props.createSubscription}
-                setWantToSubscribe={setWantToSubscribe}
-                translate={props.translate}
-              />
-            </Elements>
+            <>
+              {productPresentation}
+              <Elements stripe={stripePromise}>
+                <SubscribeForm
+                  createSubscription={props.createSubscription}
+                  setWantToSubscribe={setWantToSubscribe}
+                  translate={props.translate}
+                />
+              </Elements>
+            </>
           )
         } else {
           // Show pricing options
-          result = pricing;
+          result = (
+            <>
+              {productPresentation}
+              {pricing}
+            </>
+          );
         }
       }
     }
