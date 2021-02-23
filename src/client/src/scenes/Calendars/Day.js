95819ee416a609d6df8b0faef6d3037f9fafe6a0
Carl Bolduc <contact@carlbolduc.com>
2021-02-23 16:13:27 -0500

    Change current day by clicking on td cell

--- src/client/src/scenes/Calendars/Day.js
+++ src/client/src/scenes/Calendars/Day.js
@@ -1,7 +1,12 @@
 export default function Day(props) {
 
+  function setCurrentDay() {
+    if (props.day !== null) {
+      props.setCurrentDay(props.day);
+    }
+  }
   const className = props.currentDay === props.day ? "table-primary" : "";
   return (
-    <td className={className}>{props.day}</td>
+    <td className={className} style={{cursor: "pointer"}} onClick={setCurrentDay}>{props.day}</td>
   )
 }
