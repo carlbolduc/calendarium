49186f88a265966398ec396d1f886993afcc88a5
Carl Bolduc <contact@carlbolduc.com>
2021-02-28 10:58:57 -0500

    Highlight nav active section

--- src/client/src/components/Header/Header.js
+++ src/client/src/components/Header/Header.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Account from './Account';
 import Language from './Language';
 
 export default function Header(props) {
+  let location = useLocation();
+
+  function navClassName(path) {
+    return location.pathname.indexOf(path) !== -1 ? "nav-link active" : "nav-link";
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -11,7 +15,6 @@ export default function Header(props) {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
-        {/* TODO: set className="nav-link active" when we are on that page */}
         <div className="collapse navbar-collapse" id="main-navbar">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {/* TODO: reenable My events section at a later dev phase */}
@@ -19,10 +22,10 @@ export default function Header(props) {
               ? <li className="nav-item"><Link className="nav-link" to="/my-events">{props.translate("My events")}</Link></li>
               : null} */}
             {props.authenticated
-              ? <li className="nav-item"><Link className="nav-link" to="/my-calendars">{props.translate("My calendars")}</Link></li>
+              ? <li className="nav-item"><Link className={navClassName("/my-calendars")} to="/my-calendars">{props.translate("My calendars")}</Link></li>
               : null}
             <li className="nav-item">
-              <Link className="nav-link" to="/public-calendars">{props.translate("Public calendars")}</Link>
+              <Link className={navClassName("/public-calendars")} to="/public-calendars">{props.translate("Public calendars")}</Link>
             </li>
           </ul>
           <ul className="navbar-nav mb-2 mb-lg-0">
