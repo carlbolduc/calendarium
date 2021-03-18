9ce711bfe009bc28915c6707cd15c1a4e4dccd72
Carl Bolduc <contact@carlbolduc.com>
2021-03-18 13:10:00 -0400

    Center the loading animation

--- src/client/src/scenes/Embed/Embed.js
+++ src/client/src/scenes/Embed/Embed.js
@@ -40,8 +40,10 @@ export default function Embed(props) {
     let result;
     if (loading) {
       result = (
-        <div className="spinner-grow" role="status">
-          <span className="visually-hidden">Loading...</span>
+        <div className="d-flex justify-content-center">
+          <div className="spinner-grow" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
         </div>
       );
     } else if (props.calendar.calendarId === null) {
