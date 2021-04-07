48a580053e7bd1ada839ee9596d801ad56c7193b
Carl Bolduc <contact@carlbolduc.com>
2021-04-07 09:45:09 -0400

    Show dots to anonymous users

--- src/client/src/services/DotHook.js
+++ src/client/src/services/DotHook.js
@@ -5,22 +5,29 @@ export function useDot(token) {
   const [dots, setDots] = useState([]);
 
   const getDots = useCallback((q, cb) => {
-    if (token !== null) {
-      axios({
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-        url: `${process.env.REACT_APP_API}/calendars/dots?q=${q}`,
-      }).then(res => {
+    const url =
+      token !== null
+        ? `${process.env.REACT_APP_API}/calendars/dots?q=${q}`
+        : `${process.env.REACT_APP_API}/public/dots?q=${q}`;
+    const headers =
+      token !== null
+        ? {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          }
+        : { "Content-Type": "application/json" };
+    axios({
+      method: "GET",
+      headers: headers,
+      url: url,
+    })
+      .then((res) => {
         setDots(res.data);
-      }).catch(err => {
+      })
+      .catch((err) => {
         console.log("THIS SHOULD NEVER HAPPEN, error in 'getDots' from 'useDot' hook");
         console.log(err.response);
       });
-    }
-
   }, [token]);
 
   const getPublicDots = useCallback((q, cb) => {
